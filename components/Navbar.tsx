411dec8587109c5bd8a941fb9271a0f18a05ef13
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2023-03-30 06:58:02 +0100

    integration de google oauth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 
 import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -12,8 +14,18 @@ const Navbar = () => {
           <Image className="cursor-pointer" src={Logo} alt="logo" />
         </div>
       </Link>
+      <div>SEARCH</div>
+      <div>
+        {user ? (
+          <div>Logged IN</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(response) => console.log(response)}
+            onError={() => console.log("Login Failed")}
+          />
+        )}
+      </div>
     </div>
   );
 };
-
 export default Navbar;
