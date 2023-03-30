1068b7dec37f5a3fb87f3b6d05aba228b7fe32db
JorgeDaSilva72 <73615973+JorgeDaSilva72@users.noreply.github.com>
2023-03-30 13:17:20 +0100

    Integration de la fonctionnalité des likes . Creation du composant LikeButton et de l api like.

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -142,10 +142,10 @@ const Detail = ({ postDetails }: IProps) => {
               <div className="mt-10 px-10">
                 {userProfile && (
                   <LikeButton
-                  // likes={post.likes}
-                  // flex="flex"
-                  // handleLike={() => handleLike(true)}
-                  // handleDislike={() => handleLike(false)}
+                    likes={post.likes}
+                    flex="flex"
+                    handleLike={() => handleLike(true)}
+                    handleDislike={() => handleLike(false)}
                   />
                 )}
               </div>
